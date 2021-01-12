21af97f5440003961a183d3f2b34df73e940b72c
DronAce <okeynazone@gmail.com>
2021-01-12 14:23:40 -0500

    done

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ Figure out what you need to change to give Javascript access to the `h1#header` 
 
 **YOUR NOTES**
 ```
-
+Added defer on script
 ```
 ___________
 
@@ -57,7 +57,7 @@ Now that you have access to the `h1#header` element, use Javascript to change th
 
 **YOUR NOTES**
 ```
-
+header.style.color = "blue"
 ```
 ___________
 
--- index.js
+++ index.js
@@ -4,15 +4,29 @@ console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+header.style.color = "blue"
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
-
-
-/***** Deliverable 4 *****/
-
-
+console.log('This is what the traveler object looks like: ', traveler)
+const profile = document.querySelector(" #profile ")
+profile.children[0].src = traveler.photo
+profile.children[0].alt = traveler.name
+const h2 = profile.querySelector(" h2 ")
+h2.textContent = traveler.name
+const em = profile.querySelector("em")
+em.textContent = traveler.nickname
+    /***** Deliverable 4 *****/
+traveler.animalSightings.forEach(function(postObject) {
+    const animals = document.querySelector("#animals")
+    animals.innerHTML +=
+        `<li data-id="${postObject.id}">
+            <p>${postObject.description}</p>
+            <img src= "${postObject.photo}" alt="${postObject.species}"/>
+            <a href="${postObject.link}" target="_blank">Here's a video about the ${postObject.species}</a>
+        </li>`
+})
 
 /***** Deliverable 5 *****/
+const butterfly = document.querySelector("li[data-id='3']")
+butterfly.remove()
